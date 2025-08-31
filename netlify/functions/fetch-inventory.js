215112e454c29a5642361e5e53f3fa1d02cdacc7
Dbraxton6 <daishakb@gmail.com>
2025-08-31 15:28:13 -0700

    adjust supabase access in fetch-inventory

--- netlify/functions/fetch-inventory.js
+++ netlify/functions/fetch-inventory.js
@@ -3,8 +3,8 @@
 const { createClient } = require('@supabase/supabase-js')
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 )
 
 exports.handler = async function (event, context) {
